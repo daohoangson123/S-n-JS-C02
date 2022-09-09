e6a07d4053ab16238efcc0ee8f41be2f8445e12f
daohoangson123 <daohoangson2109>
2022-09-09 18:59:59 +0700

    123

--- c02.js
+++ c02.js
@@ -14,18 +14,34 @@ let User = {
 // User.Email = "angelgrim@gmail.com";
 // User.PhoneNumber = "9876543210";
 
-console.log(User); // 01 xuat du lieu User
+// 01 xuat du lieu User
+
+console.log("ID: " + User.ID); 
+console.log("PassWord: " + User.Password); 
+console.log( "Email: " + User.Email); 
+console.log( "PhoneNumber: " + User.PhoneNumber); 
 
 // 03 so nut dien thoai
 
-let soNutDt = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9) % 10; // % chia lay du
+let s1 = parseInt(User.PhoneNumber[0]);
+let s2 = parseInt(User.PhoneNumber[1]);
+let s3 = parseInt(User.PhoneNumber[2]);
+let s4 = parseInt(User.PhoneNumber[3]);
+let s5 = parseInt(User.PhoneNumber[4]);
+let s6 = parseInt(User.PhoneNumber[5]);
+let s7 = parseInt(User.PhoneNumber[6]);
+let s8 = parseInt(User.PhoneNumber[7]);
+let s9 = parseInt(User.PhoneNumber[8]);
+let s10 = parseInt(User.PhoneNumber[9]);
+
+let soNutDt = ( s1 + s2 + s3+ s4+ s5+ s6+ s7+ s8+ s9+ s10 ) % 10;
 console.log("số nút của số đt là: " + soNutDt);
 
 console.log("\nPLAYER");
 
 let Player = {
     Name: "Frank Lampard",
-    Number: 8.5,
+    Number: 8,
     Skills: {
         Kick: 90,
         Pass: 95,
@@ -41,7 +57,14 @@ let Player = {
 // Player.Pass = 85;
 // Player.Headbutt = 75;
 
-console.log(Player); // 01 xuat du lieu
+// 01 xuat du lieu
+
+console.log("" + Player);
+console.log("" + Player);
+console.log("" + Player);
+console.log("" + Player);
+console.log("" + Player);
+console.log("" + Player);
 
 // 03 chi so trung binh
 
@@ -49,7 +72,7 @@ let chiSoTb = (Player.Skills.Kick + Player.Skills.Pass + Player.Skills.Headbutt)
 console.log("Chỉ số kỹ năng trung bình là: " + Math.floor(chiSoTb));
 
 hopLe = (Number.isInteger(Player.Number)) && 0 < Player.Number && Player.Number < 100 ? "Số áo hợp lệ" : "Số áo không hợp lệ"; // 04 so ao hop le lon hon 0 va nho hon 100 - && dung khi thoa ca 2 dieu kien
-console.log(hopLe);
+console.log("So ao: " + Player.Number + " la " + hopLe);
 // (Number.isInteger(Player.Number)) xet so nguyen
 
 console.log("\nLAPTOP");
@@ -61,8 +84,8 @@ let Laptop = {
         RAM: 8,
         HDD: 500,
         Solution: {
-            Hor: 1400,
-            Ver: 800
+            Hor: 1920,
+            Ver: 1080
         }
     }
 }
